b8e051b13ed207b3352dd12d355fa85685fbb274
andrevox99 <andrevox99@gmail.com>
2025-03-19 18:18:16 +0000

    Atualizando - Nível Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,11 +3,12 @@
     int main(){    //definindo as variaveis
     
         char estado1[50], codigo1[50], cidade1[50], estado2[50], codigo2[50], cidade2[50];
-        int populacao1, turisticos1, populacao2, turisticos2;
+        int turisticos1, turisticos2;
+        unsigned long int populacao1, populacao2;
         float area1, pib1, area2, pib2; 
 
     
-        printf("Carta 01: \n"); //entradas e saidas de dados da carta 01
+        printf("*** Carta 01: *** \n"); //entradas e saidas de dados da carta 01
 
         printf("Digite o Estado: \n");
         scanf("%s", &estado1);
@@ -19,7 +20,7 @@
         scanf("%s", &cidade1);
 
         printf("Digite a População da Cidade: \n");
-        scanf("%d", &populacao1);
+        scanf("%lu", &populacao1);
 
         printf("Digite a Área da Cidade: \n");
         scanf("%f", &area1);
@@ -32,7 +33,7 @@
 
 
 
-        printf("Carta 02: \n"); //entradas e saidas de dados da carta 02
+        printf("*** Carta 02: *** \n"); //entradas e saidas de dados da carta 02
 
         printf("Digite o Estado: \n");
         scanf("%s", &estado2);
@@ -44,7 +45,7 @@
         scanf("%s", &cidade2);
 
         printf("Digite a População da Cidade: \n");
-        scanf("%d", &populacao2);
+        scanf("%lu", &populacao2);
 
         printf("Digite a Área da Cidade: \n");
         scanf("%f", &area2);
@@ -55,23 +56,26 @@
         printf("Digite a Quantidade de Pontos Turísticos da Cidade: \n");
         scanf("%d", &turisticos2);
 
-
+        
         float densidadepopulacional1 = (float) populacao1 / area1;
-        float pibpercapta1 = pib1 / (float) populacao1; 
+        float pibpercapta1 = pib1 / (float) populacao1;
+        float superpoder1 = populacao1 + area1 + pib1 + turisticos1 + pibpercapta1 - densidadepopulacional1;
         printf("*** Carta 01 *** \n"); // saidas de dados após entradas de dados
         printf("Estado: %s \n", estado1);
         printf("Código: %s \n", codigo1);
         printf("Nome da Cidade: %s \n", cidade1);
-        printf("População: %d \n", populacao1);
+        printf("População: %lu \n", populacao1);
         printf("Área: %f \n", area1);
         printf("PIB: %f \n", pib1);
         printf("Número de Pontos Turísticos: %d \n", turisticos1);
         printf("Densidade Populacional: %f \n", densidadepopulacional1);
-        printf("PIB Per Capta: %f \n \n \n", pibpercapta1);
+        printf("PIB Per Capta: %f \n", pibpercapta1);
+        printf("Super PODER: %f \n \n \n", superpoder1);
         
 
         float densidadepopulacional2 = (float) populacao2 / area2;
-        float pibpercapta2 = pib2 / (float) populacao2; 
+        float pibpercapta2 = pib2 / (float) populacao2;
+        float superpoder2 = populacao2 + area2 + pib2 + turisticos2 + pibpercapta2 - densidadepopulacional2;
         printf("*** Carta 02 *** \n");
         printf("Estado: %s \n", estado2);
         printf("Código: %s \n", codigo2);
@@ -82,6 +86,20 @@
         printf("Número de Pontos Turísticos: %d \n", turisticos2);
         printf("Densidade Populacional: %f \n", densidadepopulacional2);
         printf("PIB Per Capta: %f \n", pibpercapta2);
+        printf("Super PODER: %f \n \n \n", superpoder2);
+
+        // comparando resultados
+        int resultado1, resultado2, resultado3, resultado4, resultado5, resultado6, resultado7;
+
+        printf("*** Comparação de Cartas: *** \n");
+        printf("População: %d\n", resultado1 = (float)populacao1 > (float)populacao2); // conversão explícita
+        printf("Área: %d\n", resultado2 = area1 > area2);
+        printf("PIB: %d\n", resultado3 = pib1 > pib2);
+        printf("Pontos Turísticos: %d\n", resultado4 = turisticos1 > turisticos2);
+        printf("Densidade Populacional: %d\n", resultado5 = densidadepopulacional1 < densidadepopulacional2);
+        printf("PIB Per Capta: %d\n", resultado6 = pibpercapta1 > pibpercapta2);
+        printf("Super Poder: %d\n", resultado7 = superpoder1 > superpoder2);
+        
 
         return 0;
 
